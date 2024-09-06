3e78b102d73b8711903009b2dad39b758e05c4bb
AnhBui204 <155693129+AnhBui204@users.noreply.github.com>
2024-09-06 12:02:26 +0700

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,11 +1,11 @@
 
-CREATE DATABASE DoAnDBSWP391;
-USE DoAnDBSWP391;
+CREATE DATABASE b;
+USE b;
 
--- Table for Customers
-CREATE TABLE Customers (
-    CustomerID CHAR(6) PRIMARY KEY,
-    CustomerName VARCHAR(50) UNIQUE,
+-- Table for User
+CREATE TABLE Users (
+    UserID CHAR(6) PRIMARY KEY,
+    UserName VARCHAR(50) UNIQUE,
     Pass VARCHAR(80),
     Email VARCHAR(100) UNIQUE,
     Role NVARCHAR(20) CHECK (Role IN ('Admin', 'User')) DEFAULT 'User',
@@ -20,9 +20,8 @@ CREATE TABLE Customers (
 CREATE TABLE Booking(
     CustomerID CHAR(6),
     BookingDate DATE,
-    TicketNumber INT,
-    PRIMARY KEY (CustomerID, BookingDate),
-    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+    TicketID CHAR(6) PRIMARY KEY,
+    FOREIGN KEY (CustomerID) REFERENCES Users(UserID)
 );
 
 -- Table for Theatres
@@ -50,15 +49,6 @@ CREATE TABLE Seats(
     FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID)
 );
 
--- Table for Shows (linked to Movies)
-CREATE TABLE Shows(
-    ShowID CHAR(6) PRIMARY KEY,
-    StartTime DATETIME,  -- Show start time
-    EndTime DATETIME,    -- Show end time
-    MovieID CHAR(6),     -- Link to Movie
-    FOREIGN KEY (MovieID) REFERENCES Movies(MovieID)
-);
-
 -- Table for Movies
 CREATE TABLE Movies (
     MovieID CHAR(6) PRIMARY KEY,
@@ -66,6 +56,15 @@ CREATE TABLE Movies (
     Director VARCHAR(50),
     movieType VARCHAR(50),
     releaseDate DATE
+);
+
+-- Table for Shows (linked to Movies)
+CREATE TABLE Shows(
+    ShowID CHAR(6) PRIMARY KEY,
+    StartTime DATETIME,  -- Show start time
+    EndTime DATETIME,    -- Show end time
+    MovieID CHAR(6),     -- Link to Movie
+    FOREIGN KEY (MovieID) REFERENCES Movies(MovieID)
 );
 
 -- Table for Actors
@@ -83,26 +82,12 @@ CREATE TABLE MovieActors (
     FOREIGN KEY (ActorID) REFERENCES Actors(ActorID)
 );
 
--- Table for Tickets (linked to Customers, Seats, Shows)
-CREATE TABLE Tickets (
-    TicketID CHAR(6) PRIMARY KEY,
-    CustomerID CHAR(6),
-    SeatID CHAR(6),
-    ShowID CHAR(6),
-    BookingDate DATETIME,
-    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
-    FOREIGN KEY (SeatID) REFERENCES Seats(SeatID),
-    FOREIGN KEY (ShowID) REFERENCES Shows(ShowID)
-);
-
 -- Table for Vouchers (optional for discounts, offers, etc.)
 CREATE TABLE Vouchers (
     VoucherID CHAR(6) PRIMARY KEY,
-    MovieID CHAR(6), 
-    OfferID CHAR(6),
-    Price MONEY,
+    TicketID CHAR(6), 
+    Price MONEY CHECK (Price >= 0),
     ExpiryDate DATE,
-    FOREIGN KEY (MovieID) REFERENCES Movies(MovieID)
 );
 
 -- Foods and Drinks Table (Optional)
@@ -112,26 +97,20 @@ CREATE TABLE FoodsAndDrinks (
     Price MONEY
 );
 
--- Admin Table (for managing system, linked with Tickets and Foods)
-CREATE TABLE Admins (
-    AdminID CHAR(6) PRIMARY KEY,
-    AdminPass VARCHAR(80), -- Ensure password is hashed in application logic
-    Role NVARCHAR(20) CHECK (Role IN ('Admin')) DEFAULT 'Admin'
-);
 
--- Admins selling Tickets and Foods and Drinks
-CREATE TABLE AdminTicketSales (
-    AdminID CHAR(6),
-    TicketID CHAR(6),
-    PRIMARY KEY (AdminID, TicketID),
-    FOREIGN KEY (AdminID) REFERENCES Admins(AdminID),
-    FOREIGN KEY (TicketID) REFERENCES Tickets(TicketID)
-);
-
-CREATE TABLE AdminFoodSales (
-    AdminID CHAR(6),
-    FoodID CHAR(6),
-    PRIMARY KEY (AdminID, FoodID),
-    FOREIGN KEY (AdminID) REFERENCES Admins(AdminID),
-    FOREIGN KEY (FoodID) REFERENCES FoodsAndDrinks(FoodID)
+-- Table for Tickets (linked to Customers, Seats, Shows)
+CREATE TABLE Tickets (
+    TicketID CHAR(6) PRIMARY KEY,
+    CustomerID CHAR(6),
+    SeatID CHAR(6),
+    ShowID CHAR(6),
+	ComboID CHAR(6),
+    BookingDate DATETIME,
+	VoucherID CHAR(6),
+	Price MONEY CHECK (Price >= 0),
+    FOREIGN KEY (TicketID) REFERENCES Booking(TicketID),
+    FOREIGN KEY (SeatID) REFERENCES Seats(SeatID),
+    FOREIGN KEY (ShowID) REFERENCES Shows(ShowID),
+	FOREIGN KEY (ComboID) REFERENCES FoodsAndDrinks(ComboID),
+	FOREIGN KEY (VoucherID) REFERENCES Vouchers(VoucherID)
 );
