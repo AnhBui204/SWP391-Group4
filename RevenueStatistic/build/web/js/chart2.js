b5ef42c6d739b93aa1172a4510dca5fe60be3e99
Meoanvung <155882817+Meoanvung@users.noreply.github.com>
2024-10-20 13:09:49 +0700

    thống kê doanh thu . Admin_ revenue_Statistic

---
+++ RevenueStatistic/build/web/js/chart2.js
@@ -0,0 +1,44 @@
+var ctx2 = document.getElementById('doughnut').getContext('2d');
+var myChart2 = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Starlight Đà Nẵng', 
+            'Rio Đà Nẵng', 
+            'CGV Plaza', 
+            'CGV Vincom Đà Nẵng', 
+            'Lotte Đà Nẵng', 
+            'Galaxy Đà Nẵng', 
+            'Metiz Cinema', 
+            'IF Đà Nẵng'
+        ],
+        datasets: [{
+            label: 'Cinema',
+            data: [10, 15, 20, 25, 12, 8, 6, 4], 
+            backgroundColor: [
+                'rgba(41, 155, 99, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(120, 46, 139, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderColor: [
+                'rgba(41, 155, 99, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(120, 46, 139, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
