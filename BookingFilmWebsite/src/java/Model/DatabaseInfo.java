6fe86074ac8ffd7270f36c71030a24417bbc887c
Nguyen Hong Dien <132563944+nguyenhongdien@users.noreply.github.com>
2024-10-23 00:22:05 +0700

    Delete BookingFilmWebsite directory

--- BookingFilmWebsite/src/java/Model/DatabaseInfo.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-
-public interface DatabaseInfo {
-    public static String DRIVERNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static String DBURL="jdbc:sqlserver://LAPTOP-DQSDNPL1\\MSSQLSERVER01;databaseName=a;encrypt=false;trustServerCertificate=false;loginTimeout=30";
-    public static String USERDB="sa";
-    public static String PASSDB="123";
-}
